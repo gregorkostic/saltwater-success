4b1b68d7fa00d092c6e47195144d26563a788191
gregorkostic <gregor.kostic11@gmail.com>
2024-05-20 23:21:29 +0200

    Update 1.2.7 - Firebase

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
 import {
   doc,
@@ -6,44 +5,26 @@ import {
   getDoc,
   setDoc,
   getDocs,
-  query,
-  where,
   getFirestore,
   collection,
-  updateDoc,
+  query,
+  where,
   deleteDoc,
-  orderBy,
 } from "firebase/firestore/lite";
 import {
   getAuth,
-  updatePassword,
   signOut,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   sendPasswordResetEmail,
-  confirmPasswordReset,
-  beforeAuthStateChanged,
   onAuthStateChanged,
-  deleteUser,
 } from "firebase/auth";
-import {
-  getStorage,
-  ref,
-  uploadBytes,
-  getDownloadURL,
-  listAll,
-  getMetadata,
-  deleteObject,
-} from "firebase/storage";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAjFq6_3WOD4U4AK9znLm_JYJPhDd1mIjE",
   authDomain: "saltwater-success.firebaseapp.com",
@@ -53,6 +34,8 @@ const firebaseConfig = {
   appId: "1:251162312716:web:2a18c32890b3b4021f6e29",
   measurementId: "G-J63S7FV622",
 };
+
+//firebase.initializeApp(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -64,8 +47,8 @@ export {
   app,
   auth,
   storage,
-  getAuth,
   db,
+  getAuth,
   doc,
   getDoc,
   setDoc,
@@ -78,17 +61,9 @@ export {
   uploadBytes,
   getDownloadURL,
   signOut,
-  updatePassword,
   collection,
-  getDocs,
   query,
+  getDocs,
   where,
-  updateDoc,
-  listAll,
-  deleteUser,
   deleteDoc,
-  getMetadata,
-  deleteObject,
-  orderBy,
-  getStorage,
 };
